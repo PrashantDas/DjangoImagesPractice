5ed872ef464294ef272ee7e49ba3492e707cdc48
PrashantDas <78946070+PrashantDas@users.noreply.github.com>
2023-07-22 13:04:05 +0530

    Update settings.py

--- project1/settings.py
+++ project1/settings.py
@@ -142,7 +142,8 @@ STORAGES = {"default": {"BACKEND": "storages.backends.s3boto3.S3Boto3Storage"},
 # AWS Bucket
 AWS_ACCESS_KEY_ID = os.environ["MY_AWS_ACCESS_KEY_ID"]
 AWS_SECRET_ACCESS_KEY = os.environ["MY_AWS_SECRET_ACCESS_KEY"]
-AWS_STORAGE_BUCKET_NAME = 'paintme'
+AWS_STORAGE_BUCKET_NAME = 'newgalleryart'
+AWS_S3_REGION_NAME = 'ap-south-1'
 AWS_QUERYSTRING_AUTH = False
 AWS_DEFAULT_ACL = None
 AWS_S3_FILE_OVERWRITE = False
