976260906e6ab4f43d8b0a59501168d211b93c03
PrashantDas <78946070+PrashantDas@users.noreply.github.com>
2023-07-26 15:42:16 +0530

    Update settings.py

--- project1/settings.py
+++ project1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-iwz2dcp_b0d7hetjo&s%^8r_ynppso19t(jxo6r9q4pdnmt!i7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['web-production-49c9.up.railway.app', '127.0.0.1', 'localhost']
 
 
 # Application definition
